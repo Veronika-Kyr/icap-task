6ca940654db8cfee6f76552027780b2fb135c6e3
Veronika-Kyr <106926961+Veronika-Kyr@users.noreply.github.com>
2023-10-24 16:32:44 +0300

    Correct paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
+  server: {
+    open: true,
+    https: true,
+    port: 5500,
+    host: 'localhost'
+  },
   define: {
     'process.env': {},
   },
-
-  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react(), basicSsl()],
 })
