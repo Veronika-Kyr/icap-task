e1ead466ed377939701964bd789124ad13cd1a14
Veronika-Kyr <106926961+Veronika-Kyr@users.noreply.github.com>
2023-10-24 15:55:17 +0300

    Correct routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './app/store.ts';
 import App from './App.tsx'
@@ -7,9 +7,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </Provider>
   ,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {},
+  },
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
 })
